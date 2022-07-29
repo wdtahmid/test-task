7d839de43abe77558cd0dbc97a677bdac2e08e4c
Tahmid Hasan <wdtahmid@gmail.com>
2022-07-29 19:01:58 +0600

    Form created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Application from './Application/Application';
 
 function App() {
   return (
     <div className="App">
       <h1>Receipts Details</h1>
+      <Application />
     </div>
   );
 }
--- src/Application/Application.js
+++ src/Application/Application.js
@@ -3,8 +3,39 @@ import React from 'react';
 const Application = () => {
     return (
         <>
-            <div>
+            <div style={{ width: '400px', margin: '0 auto' }}>
+                <form action="" style={{ textAlign: 'left' }}>
+                    <div style={{ display: 'flex', gap: '20px' }}>
+                        <div style={{ width: '200px' }}>Date <span style={{ color: 'red' }}>*</span></div>
+                        <div><input type="date" name="date" id="" required /></div>
+                    </div>
+                    <div style={{ display: 'flex', gap: '20px' }}>
+                        <div style={{ width: '200px' }}>Amount <span style={{ color: 'red' }}>*</span></div>
+                        <div><input type="number" name="number" id="" required /></div>
+                    </div>
+                    <div style={{ display: 'flex', gap: '20px' }}>
+                        <div style={{ width: '200px' }}>Payment Mode <span style={{ color: 'red' }}>*</span></div>
+                        <div>
+                            <select name="payment_mode" id="">
+                                <option value="cash">Cash</option>
+                                <option value="check">Check</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div style={{ display: 'flex', gap: '20px' }}>
+                        <div style={{ width: '200px' }}>Remark </div>
+                        <div><input type="text" name="reamrk" id="" /></div>
+                    </div>
+                    <div style={{ display: 'flex', gap: '20px' }}>
+                        <div style={{ width: '200px' }}></div>
+                        <div>
+                            <input type="submit" value="Submit" />
+                            <input type="reset" value="Reset" />
+                        </div>
+                    </div>
 
+
+                </form>
             </div>
         </>
     );
